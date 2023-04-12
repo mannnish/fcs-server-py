05c8cf56c571790c08fe79193189dce49e474e17
mannnish <mk458978@gmail.com>
2023-04-12 09:24:54 +0530

    1.0.2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def home():
 
 @app.route('/v', methods=['GET'])
 def version():
-    message = {'version' : '1.0.2'}
+    message = {'version' : '1.0.3'}
     json_dump = json.dumps(message)
     response = make_response(json_dump)
     response.headers['Content-Type'] = 'application/json'

f84a2f0f61d5b12b38b3f67119a467fccf997bf8
mannnish <mk458978@gmail.com>
2023-05-16 00:01:36 +0530

    countries added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from markdown import markdown
 from flask import *
 import json, csv
 import numpy as np, pandas as pd
@@ -5,20 +6,11 @@ import numpy as np, pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 app = Flask(__name__)
-
 print("server started")
 
 @app.route('/', methods=['GET'])
 def home():
-    message = {'status' : 200, 'message' : 'Running'}
-    json_dump = json.dumps(message)
-    response = make_response(json_dump)
-    response.headers['Content-Type'] = 'application/json'
-    return response
-
-@app.route('/v', methods=['GET'])
-def version():
-    message = {'version' : '1.1.1', 'desc' : 'prediction check'}
+    message = {'status' : 200, 'version' : '1.2.0', 'message' : 'Running'}
     json_dump = json.dumps(message)
     response = make_response(json_dump)
     response.headers['Content-Type'] = 'application/json'
@@ -42,7 +34,6 @@ def py():
     
     dfc = pd.read_csv("data/{a}_crop.csv".format(a=countryStr))
     dfc.columns = ['Entity','Code','Year','Temp','Wheat','Rice','Bananas','Maize','Soybeans','Potatoes','Beans','Peas','Cassava','Cocoa','Barley']
-    # lr = lrBasic(dfc, cropStr)
     X = dfc[['Year', 'Temp']]
     y = dfc[cropStr]
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=False)
@@ -53,7 +44,8 @@ def py():
         'country' : countryStr, 
         'crop' : cropStr, 
         'start' : yearPredStart, 
-        'end' : yearPredEnd - 1, 
+        'end' : yearPredEnd - 1,
+        'unit' : "tonnes per hectare", 
         'temperature' : resTemp,
         'production' : resProd.tolist()
         }
--- readme.md
+++ readme.md
@@ -18,12 +18,14 @@
 
 
 ### installation and setup
-1.  dev : to install python3 and pip3
+1.  dev : to install python3 and pip3 and run
     ```sh
     sudo apt-get python3
     sudo apt install python3-pip
     pip install pandas numpy tensorflow sklearn
     pip3 install -U scikit-learn scipy matplotlib seaborn
+
+    flask run
     ```
 1. prod : run `pip freeze > requirements.txt` in your console to create a requirements.txt file, and if while deploying you get any errors, you can remove the version numbers from the requirements.txt file and try deploying again.
 1. prod : server will run this script before running the project to install the dependencies. Save this script as __build.sh__ in the root directory of the project
